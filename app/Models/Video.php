aa85f0456e1127f222a793b0138db853b37542e4
Marcelo Corrêa <marcelocorrea229@gmail.com>
2021-01-29 12:47:09 -0300

    primeiras modificações no model de vídeos para suportar os uploads

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use MarceloCorrea\Uuid\Uuid;
+use Throwable;
 
 class Video extends Model
 {
@@ -28,6 +30,61 @@ class Video extends Model
         'year_launched' => 'integer',
         'duration'      => 'integer',
     ];
+
+    /**
+     * @param array $attributes
+     *
+     * @return Builder|Model
+     * @throws Throwable
+     */
+    public static function create(array $attributes = [])
+    {
+        try {
+            \DB::beginTransaction();
+            $obj = static::query()->create($attributes);
+
+            // upload aqui
+            // excluir arquivos antigos
+
+            \DB::commit();
+
+            return $obj;
+        } catch (\Exception $e) {
+
+            if (isset($obj)) {
+                // excluir arquivos de uploads
+            }
+            \DB::rollBack();
+            throw $e;
+        }
+    }
+
+    /**
+     * @param array $attributes
+     * @param array $options
+     *
+     * @return bool
+     * @throws Throwable
+     */
+    public function update(array $attributes = [], array $options = []): bool
+    {
+        try {
+            \DB::beginTransaction();
+            $saved = parent::update($attributes, $options);
+            if ($saved) {
+
+            }
+            \DB::commit();
+
+            return $saved;
+        } catch (\Exception $e) {
+
+            // excluir arquivos de uploads
+
+            \DB::rollBack();
+            throw $e;
+        }
+    }
 
     public function categories(): BelongsToMany
     {
