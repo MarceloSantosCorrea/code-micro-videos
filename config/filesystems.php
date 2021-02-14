c0cb4183240ad0f12479a055ed35aa46c0e79944
Marcelo Corrêa <marcelocorrea229@gmail.com>
2021-02-13 22:16:47 -0300

    rename credentials file

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
         'gcs' => [
             'driver'          => 'gcs',
             'project_id'      => env('GOOGLE_CLOUD_PROJECT_ID', 'your-project-id'),
-            'key_file'        => storage_path('credentials/google/silver-course-304718-83d72aac3d42.json'),
+            'key_file'        => storage_path('credentials/google/service-account.json'),
             'bucket'          => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'your-bucket'),
             'path_prefix'     => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
